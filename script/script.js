d4604091395a98c18ffcb0f8231ae39b23cf8f4a
gentlemanhorse <138687107+gentlemanhorse@users.noreply.github.com>
2023-11-23 13:43:39 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -6,7 +6,7 @@ let person = {
     mail: "",
     content: "",
     feedback: "",
-
+    date: ""
 };
 
 function submitData(){
@@ -15,14 +15,42 @@ function submitData(){
     person.mail = document.getElementById("email").value;
     person.content = document.getElementById("content").value;
     person.feedback = document.getElementById("feedback").value;
+    person.date = new Date().toDateString();
+    if (person.name === '') {
+        alert("Você precisa inserir o nome");
+    }
     
-    alert(person.name+" "+person.age+" "+person.mail+" "+person.content+" "+person.feedback);
+    else{
+        addElemento(person);
+    }
+    
 }
 
 function cleanAll(){
     document.getElementById("username").value = "";
-    document.getElementById("age").value = 18;
+    document.getElementById("age").value = null;
     document.getElementById("email").value = "";
     document.getElementById("content").value = "sim";
     document.getElementById("feedback").value = "";
+}
+
+function addElemento(person) {
+    let li = document.createElement("li");
+    let t = document.createTextNode("COlaborador: "+person.name+" data de inscrição: "+person.date);
+    li.appendChild(t);
+    
+    document.getElementById("itemLista").appendChild(li);
+    
+    document.getElementById("tarefa").value = "";
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+    for (let i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            let div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
 }
